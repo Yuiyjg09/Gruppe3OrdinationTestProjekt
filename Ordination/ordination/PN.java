50954d26af015f48f662d685d139f520d3da3c48
MortenF <morten@faber-lyd.dk>
2018-09-11 13:22:16 +0200

    Fixet

--- Ordination/ordination/PN.java
+++ Ordination/ordination/PN.java
@@ -1,31 +1,33 @@
 package ordination;
 
-
-
 import java.time.LocalDate;
 
-public class PN extends Ordination{
+public class PN extends Ordination {
 
     private double antalEnheder;
 
-    public PN( LocalDate startDen,  LocalDate slutDen) {
+    public PN(LocalDate startDen, LocalDate slutDen) {
         super(startDen, slutDen);
     }
 
     /**
-     * Registrerer at der er givet en dosis paa dagen givesDen
-     * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
+     * Registrerer at der er givet en dosis paa dagen givesDen Returnerer true hvis
+     * givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
      * Retrurner false ellers og datoen givesDen ignoreres
+     *
      * @param givesDen
      * @return
      */
     public boolean givDosis(LocalDate givesDen) {
         // TODO
-        if (givesDen.isAfter(this.getStartDen()) && givesDen.isBefore(this.getSlutDen())) {
+        if (givesDen.isAfter(getStartDen()) && givesDen.isBefore(getSlutDen())) {
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
 
+    @Override
     public double doegnDosis() {
         // TODO
         return 0.0;
@@ -36,7 +38,7 @@ public class PN extends Ordination{
         return this.getClass().getName();
     }
 
-
+    @Override
     public double samletDosis() {
         // TODO
         return 0.0;
@@ -44,11 +46,12 @@ public class PN extends Ordination{
 
     /**
      * Returnerer antal gange ordinationen er anvendt
+     *
      * @return
      */
     public int getAntalGangeGivet() {
         // TODO
-        return-1;
+        return -1;
     }
 
     public double getAntalEnheder() {
