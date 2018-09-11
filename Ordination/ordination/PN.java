3153871e3f4438ca59ace3cc027f4bac92b10269
Martin <martndk@protonmail.com>
2018-09-11 13:06:18 +0200

    Jeg prøver

--- Ordination/ordination/PN.java
+++ Ordination/ordination/PN.java
@@ -1,16 +1,13 @@
 package ordination;
 
-import com.sun.istack.internal.NotNull;
-
 import java.time.LocalDate;
 
-public class PN extends Ordination{
+public class PN extends Ordination {
 
-<<<<<<< HEAD
 	private double antalEnheder;
 
-	public PN(double antalEnheder) {
-		this.antalEnheder = antalEnheder;
+	public PN(LocalDate startDen, LocalDate slutDen) {
+		super(startDen, slutDen);
 	}
 
 	/**
@@ -23,17 +20,20 @@ public class PN extends Ordination{
 	 */
 	public boolean givDosis(LocalDate givesDen) {
 		// TODO
-		return false;
+		if (givesDen.isAfter(this.getStartDen()) && givesDen.isBefore(this.getSlutDen())) {
+			return true;
+		} else
+			return false;
 	}
 
-	/**
-	 * (antal gange ordinationen er anvendt * antal enheder) / (antal dage mellem
-	 * første og sidste givning)
-	 * 
-	 * @return
-	 */
 	public double doegnDosis() {
+		// TODO
 		return 0.0;
+	}
+
+	@Override
+	public String getType() {
+		return this.getClass().getName();
 	}
 
 	public double samletDosis() {
@@ -54,59 +54,8 @@ public class PN extends Ordination{
 	public double getAntalEnheder() {
 		return antalEnheder;
 	}
+
+	public void setAntalEnheder(double antalEnheder) {
+		this.antalEnheder = antalEnheder;
+	}
 }
-=======
-    private double antalEnheder;
-
-    public PN(@NotNull LocalDate startDen, @NotNull LocalDate slutDen) {
-        super(startDen, slutDen);
-    }
-
-    /**
-     * Registrerer at der er givet en dosis paa dagen givesDen
-     * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
-     * Retrurner false ellers og datoen givesDen ignoreres
-     * @param givesDen
-     * @return
-     */
-    public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        if (givesDen.isAfter(this.getStartDen()) && givesDen.isBefore(this.getSlutDen())) {
-            return true;
-        } else return false;
-    }
-
-    public double doegnDosis() {
-        // TODO
-        return 0.0;
-    }
-
-    @Override
-    public String getType() {
-        return this.getClass().getName();
-    }
-
-
-    public double samletDosis() {
-        // TODO
-        return 0.0;
-    }
-
-    /**
-     * Returnerer antal gange ordinationen er anvendt
-     * @return
-     */
-    public int getAntalGangeGivet() {
-        // TODO
-        return-1;
-    }
-
-    public double getAntalEnheder() {
-        return antalEnheder;
-    }
-
-    public void setAntalEnheder(double antalEnheder) {
-        this.antalEnheder = antalEnheder;
-    }
-}
->>>>>>> master
