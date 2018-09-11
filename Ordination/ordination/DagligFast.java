453d3a1ef4ea5cf6c664ab68e618cbb3fcad1ad1
Martin <martndk@protonmail.com>
2018-09-11 13:35:47 +0200

    Lidt mere rettelser.

--- Ordination/ordination/DagligFast.java
+++ Ordination/ordination/DagligFast.java
@@ -1,70 +1,65 @@
 package ordination;
 
-
 import java.time.*;
 
 public class DagligFast extends Ordination {
 
-    private Dosis[] doses = new Dosis[4];
+	private Dosis[] doses = new Dosis[4];
 
-    public DagligFast( LocalDate startDen,  LocalDate slutDen) {
-        super(startDen, slutDen);
-    }
+	public DagligFast(LocalDate startDen, LocalDate slutDen) {
+		super(startDen, slutDen);
+	}
 
-    public void opretDosis(LocalTime tid, double antal) {
-        Dosis dosis = new Dosis(tid, antal);
-        boolean free = false;
-        for (int i = 0; i < doses.length; i++) {
-            if (doses[i] == null) {
-                free = true;
-            }
-        }
+	public void opretDosis(LocalTime tid, double antal) {
+		Dosis dosis = new Dosis(tid, antal);
+		boolean free = false;
+		for (int i = 0; i < doses.length; i++) {
+			if (doses[i] == null) {
+				free = true;
+			}
+		}
 
-        if (free) {
-            this.addDosis(dosis);
-        }
-    }
+		if (free) {
+			this.addDosis(dosis);
+		}
+	}
 
-    @Override
-    public double samletDosis() {
-        return 0;
-    }
+	@Override
+	public double samletDosis() {
+		return 0;
+	}
 
-    @Override
-    public double doegnDosis() {
-        return 0;
-    }
+	@Override
+	public double doegnDosis() {
+		return 0;
+	}
 
-    @Override
-    public String getType() {
-        return this.getClass().getName();
-    }
+	@Override
+	public String getType() {
+		return this.getClass().getName();
+	}
 
-    public Dosis[] getDoser() {
-        return doses;
-    }
+	public Dosis[] getDoser() {
+		return doses;
+	}
 
-    public void setDoses(Dosis[] doses) {
-        this.doses = doses;
-    }
+	public void setDoses(Dosis[] doses) {
+		this.doses = doses;
+	}
 
-    public void addDosis(Dosis dosis) {
-        for (int i = 0; i < doses.length; i++) {
-            if (doses[i] == null) {
-                doses[i] = dosis;
-            }
-        }
-    }
+	public void addDosis(Dosis dosis) {
+		for (int i = 0; i < doses.length; i++) {
+			if (doses[i] == null) {
+				doses[i] = dosis;
+			}
+		}
+	}
 
-    public void removeDosis(Dosis dosis) {
-        for (int i = 0; i < doses.length; i++) {
-            if (doses[i] == dosis) {
-                doses[i] = null;
-            }
-        }
-    }
-
-
-
-
+	public void removeDosis(Dosis dosis) {
+		for (int i = 0; i < doses.length; i++) {
+			if (doses[i] == dosis) {
+				doses[i] = null;
+			}
+		}
+	}
 }
--- Ordination/ordination/DagligSkaev.java
+++ Ordination/ordination/DagligSkaev.java
@@ -1,48 +1,46 @@
 package ordination;
 
-
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination {
 
-    private ArrayList<Dosis> doses = new ArrayList<>();
+	private ArrayList<Dosis> doses = new ArrayList<>();
 
-    public DagligSkaev( LocalDate startDen,  LocalDate slutDen) {
-        super(startDen, slutDen);
-    }
+	public DagligSkaev(LocalDate startDen, LocalDate slutDen) {
+		super(startDen, slutDen);
+	}
 
-    public void opretDosis(LocalTime tid, double antal) {
-        Dosis dosis = new Dosis(tid, antal);
-        this.addDosis(dosis);
-    }
+	public void opretDosis(LocalTime tid, double antal) {
+		Dosis dosis = new Dosis(tid, antal);
+		this.addDosis(dosis);
+	}
 
-    public ArrayList<Dosis> getDoser() {
-        return new ArrayList<Dosis>(doses);
-    }
+	public ArrayList<Dosis> getDoser() {
+		return new ArrayList<Dosis>(doses);
+	}
 
-    public void addDosis(Dosis dosis) {
-        doses.add(dosis);
-    }
+	public void addDosis(Dosis dosis) {
+		doses.add(dosis);
+	}
 
-    public void removeDosis(Dosis dosis) {
-        doses.remove(dosis);
-    }
+	public void removeDosis(Dosis dosis) {
+		doses.remove(dosis);
+	}
 
-    @Override
-    public double samletDosis() {
-        return 0;
-    }
+	@Override
+	public double samletDosis() {
+		return 0;
+	}
 
-    @Override
-    public double doegnDosis() {
-        return 0;
-    }
+	@Override
+	public double doegnDosis() {
+		return 0;
+	}
 
-    @Override
-    public String getType() {
-        return this.getClass().getName();
-    }
+	@Override
+	public String getType() {
+		return this.getClass().getName();
+	}
 }
