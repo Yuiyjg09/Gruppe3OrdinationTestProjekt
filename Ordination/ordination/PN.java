0c844798e724fbfaef415450623847e030ffd1f5
Martin <martndk@protonmail.com>
2018-09-11 17:49:19 +0200

    Prøver lige en gang til

--- Ordination/ordination/PN.java
+++ Ordination/ordination/PN.java
@@ -1,61 +1,63 @@
 package ordination;
 
-
-
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-public class PN extends Ordination{
+public class PN extends Ordination {
 
-    private double antalEnheder;
+	private double antalEnheder;
+	private ArrayList<Integer> doser = new ArrayList<>();
 
-    public PN( LocalDate startDen,  LocalDate slutDen) {
-        super(startDen, slutDen);
-    }
+	public PN(LocalDate startDen, LocalDate slutDen) {
+		super(startDen, slutDen);
+	
+	}
 
-    /**
-     * Registrerer at der er givet en dosis paa dagen givesDen
-     * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
-     * Retrurner false ellers og datoen givesDen ignoreres
-     * @param givesDen
-     * @return
-     */
-    public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        if (givesDen.isAfter(this.getStartDen()) && givesDen.isBefore(this.getSlutDen())) {
-            return true;
-        } else return false;
-    }
+	/**
+	 * Registrerer at der er givet en dosis paa dagen givesDen Returnerer true hvis
+	 * givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
+	 * Retrurner false ellers og datoen givesDen ignoreres
+	 * 
+	 * @param givesDen
+	 * @return
+	 */
+	public boolean givDosis(LocalDate givesDen) {
+		if (givesDen.isAfter(this.getStartDen()) && givesDen.isBefore(this.getSlutDen())) {
+			return true;
+		} else
+			return false;
+	}
 
-    public double doegnDosis() {
-        // TODO
-        return 0.0;
-    }
+	public double doegnDosis() {
+		// TODO
+		return 0.0;
+	}
 
-    @Override
-    public String getType() {
-        return this.getClass().getName();
-    }
+	@Override
+	public String getType() {
+		return this.getClass().getName();
+	}
 
+	public double samletDosis() {
+		// TODO
+		return 0.0;
+	}
 
-    public double samletDosis() {
-        // TODO
-        return 0.0;
-    }
+	/**
+	 * Returnerer antal gange ordinationen er anvendt
+	 * 
+	 * @return
+	 */
+	public int getAntalGangeGivet() {
+		// TODO
+		return -1;
+	}
 
-    /**
-     * Returnerer antal gange ordinationen er anvendt
-     * @return
-     */
-    public int getAntalGangeGivet() {
-        // TODO
-        return-1;
-    }
+	public double getAntalEnheder() {
+		return antalEnheder;
+	}
 
-    public double getAntalEnheder() {
-        return antalEnheder;
-    }
-
-    public void setAntalEnheder(double antalEnheder) {
-        this.antalEnheder = antalEnheder;
-    }
+	public void setAntalEnheder(double antalEnheder) {
+		this.antalEnheder = antalEnheder;
+	}
 }
